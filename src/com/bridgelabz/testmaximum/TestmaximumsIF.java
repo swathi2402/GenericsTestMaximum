309bea3b730607c801d691f975c49d3e49d141a9
Swathi <swathi.h@perfios.com>
2021-08-29 20:11:59 +0530

    MODIFIED: Interface file adds float method

--- src/com/bridgelabz/testmaximum/TestmaximumsIF.java
+++ src/com/bridgelabz/testmaximum/TestmaximumsIF.java
@@ -2,4 +2,5 @@ package com.bridgelabz.testmaximum;
 
 public interface TestmaximumsIF {
 	public void findIntMaximum(Integer firstInteger, Integer secondInteger, Integer thirdInteger);
+	public void findFloatMaximum(Float firstFloat, Float secondFloat, Float thirdFloat);
 }
