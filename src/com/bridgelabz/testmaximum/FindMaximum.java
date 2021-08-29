0989e2eb8d95118f0526fe25d0cc1eb129d78d5c
Swathi <swathi.h@perfios.com>
2021-08-29 20:09:09 +0530

    MODIFIED: Changed to get value for float

--- src/com/bridgelabz/testmaximum/FindMaximum.java
+++ src/com/bridgelabz/testmaximum/FindMaximum.java
@@ -6,10 +6,10 @@ public class FindMaximum {
 
 	public static void main(String[] args) {
 		TestmaximumsIF testmaximums = new TestMaximumsImpl();
-		Integer firstInteger = 50;
-		Integer secondInteger = 40;
-		Integer thirdInteger = 30;
-		testmaximums.findIntMaximum(firstInteger, secondInteger, thirdInteger);
+		Integer firstFloat = 50;
+		Integer secondFloat = 40;
+		Integer thirdFloat = 30;
+		testmaximums.findIntMaximum(firstFloat, secondFloat, thirdFloat);
 	}
 
 }
